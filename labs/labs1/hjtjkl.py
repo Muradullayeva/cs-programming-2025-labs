8af7e8b7ca0df59bed8450c69d2a378f0de8a983
Muradullayeva <sevinchmuradullayeva29@gmail.com>
2025-10-03 13:14:51 +1000

    gyvggv

---
+++ labs/labs1/hjtjkl.py
@@ -0,0 +1,82 @@
+#1
+#ints=5
+#floats=4.8
+#strs='dssdog'
+#bools=True
+
+#2
+#name='Sevinch'
+#Age=18
+#print=(name, age)
+
+#3
+x=342
+y=56.2
+z='43'
+sum=x+y+int(z)
+print(sum)
+
+#4
+#a=3
+#b=8
+#print((a + 4 * b)(a - 3 * b)+a**2)
+
+#5
+#side1=int(input())
+#side2=int(input())
+#print(side1*side2)
+
+#6
+#print('*  *  *')
+#print(' *  *  *')
+#print('  *  *')
+
+#7
+# a=2
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a**b)
+# print(a%b)
+# print(a>b)
+# print(a<b)
+# print(a<=b)
+# print(a>=b)
+# print(a==b)
+# print(a!=b)
+
+#8
+#name1='Sevinch'
+#age1=18
+#print('Меня зовут {name1}, мне {age1} лет.')
+
+#9
+#text='Съешь еще этих мягких французских булокб да выпей чаю'
+#otd=text.split(' ')
+#print(otd)
+#again=''.join(otd)
+#print(again)
+
+#10
+#netda='Нет! Да! '*4
+#print(netda)
+
+#11
+#tri=input()
+#odi=tri.split(',')
+#print(odi)
+#c=int(odi[0] + int(odi[2])) // int(odi[1])
+#print('Результат вычисления: '+ str(s))
+
+#12
+# s=input()
+# print(s[:4])
+# reversed_s=''.join(reversed(s))
+# lasttwo1=reversed_s[:2]
+# lasttwo=''.join(reversed(lasttwo1))
+# print(lasttwo)
+# print(s[3:8])
+# print(reversed_s)
