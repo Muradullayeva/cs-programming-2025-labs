8c57c6b92e68c554daecbc9d55028b61728c2d9d
Muradullayeva <sevinchmuradullayeva29@gmail.com>
2025-10-09 22:39:46 -0700

    hgjgv

---
+++ labs/labs3/main.py
@@ -0,0 +1,48 @@
+1
+#name = input ('Севинч')
+#age = input ('18')
+#for _ in range(10) :
+#    print(f'Меня зовут {name} и мне {age} лет')
+
+#2
+#number = int(input(123456789))
+#for i in range(1, 11) :
+#    print(f'{number} * {i} = {number * i}')
+
+#3
+#for i in range(0, 101, 3) :
+
+# 4      
+#number = int(input('15'))
+#factorial = 1
+#for i in range(1, number + 1):
+#    factorial *= i
+#print(f'Факториал числа {number} равен {factorial}')
+
+#5
+#num = 20
+#while num >= 0:
+#    print(num)
+#    num -= 1
+
+#6
+#num = int(input('8'))
+#a, b = 0, 1
+#while a <= num:
+#    print(a)
+#    a, b = b, a + b
+
+#7
+#input_str = input('Привет')
+#output_str = ''
+#for index, char in  
+#enumerate(input_str, start=1) :
+#    output_str += f'{char} {index}'
+#print(output_str)
+
+#8
+#while True:
+#    user_input = 
+#input('8 15')
+#    num1, num2 = map(int, user_input . split())
+#    print(f'Сумма равна: {num1 + num2}')
